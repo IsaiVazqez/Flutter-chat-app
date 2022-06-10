7a48b0868bab4b756abbb57f62281bec1aa64a6b
IsaiVazqez <7yuvryi@gmail.com>
2022-06-09 18:57:38 -0700

    Change notifier socket service.io

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import 'package:chat/routes/routes.dart';
 import 'package:chat/core/bloc/auth_service.dart';
+import 'package:chat/core/bloc/socket_service.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => AuthService())],
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthService()),
+        ChangeNotifierProvider(create: (_) => SocketService())
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Chat App',
